71be73a993bfa8108dc3e9b8ce7a135abe47ece8
thatianne@psagotequity.co.il <amanda_fusuma@hotmail.com>
2023-08-28 17:12:08 +0100

    new expect body

--- cypress/e2e/exchangerate.cy.js
+++ cypress/e2e/exchangerate.cy.js
@@ -3,13 +3,23 @@ describe('API Test', () => {
     cy.request('https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD')
       .then((response) => {
         expect(response.status).to.equal(200);
-
+      
         const currencyCount = Object.keys(response.body.conversion_rates).length;
         cy.log('Total number of currencies:', currencyCount);
 
         const gbpCurrency = response.body.conversion_rates['GBP'];
         expect(gbpCurrency).to.exist;
         cy.log('Currency GBP:', gbpCurrency);
+
+        const verSeExisteOTempo = response.body['time_next_update_utc'];
+        expect(verSeExisteOTempo).to.exist;
+
+       const contatudo = Object.keys(response.body).length;
+       cy.log('print tudo que eu quero ver:', contatudo);
+
+       expect(response.body.conversion_rates).to.have.property('EUR');
+       expect(response.body.conversion_rates.EUR).to.be.a('number');
+
       });
   });
 });
